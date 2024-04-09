734426423d154d1d72b460e238fd47384f7cbd07
Darka2504 <hhdorian@yahoo.fr>
2024-04-09 08:59:18 +0200

    master update

--- app/src/main/java/com/example/projetmobile/ShareFragment.java
+++ app/src/main/java/com/example/projetmobile/ShareFragment.java
@@ -1,27 +1,22 @@
 package com.example.projetmobile;
 
+import android.content.Intent;
 import android.os.Bundle;
-
 import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ShareFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class ShareFragment extends Fragment {
 
-    private Button partageButton ;
+    private Button partageButton;
+    private TextView textViewMessage;
 
     public ShareFragment() {
-        // Required empty public constructor
-    }
 
+    }
 
     public static ShareFragment newInstance(String param1, String param2) {
         ShareFragment fragment = new ShareFragment();
@@ -29,22 +24,35 @@ public class ShareFragment extends Fragment {
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-    }
-
-    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+
         View rootView = inflater.inflate(R.layout.fragment_share, container, false);
 
-        // Trouver la référence du bouton dans le layout
-        partageButton = rootView.findViewById(R.id.buttonShare);
 
-        // Vous pouvez ajouter des écouteurs d'événements ou effectuer d'autres opérations avec le bouton ici
+        partageButton = rootView.findViewById(R.id.buttonShare);
+        textViewMessage = rootView.findViewById(R.id.textViewMessage);
+
+
+        partageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                partagerMessage();
+            }
+        });
 
         return rootView;
+    }
+
+    private void partagerMessage() {
+
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_TEXT, "Je m'amuse bien sur l'appli Who Am I");
+
+
+        if (intent.resolveActivity(getActivity().getPackageManager()) != null) {
+            startActivity(Intent.createChooser(intent, "Partager via"));
+        }
     }
 }
